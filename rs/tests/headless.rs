d581a3fdbff12ee0d46ce754f6b6c7bf35bed227
Waridley <Waridley64@gmail.com>
2022-12-03 00:44:25 -0600

    Merge headless & visual integration tests

--- rs/tests/headless.rs
+++
@@ -1,8 +0,0 @@
-use bevy::prelude::*;
-use sond_has::tests::*;
-
-#[bevy_main]
-fn main() {
-	let mut app = app();
-	app.run()
-}
--- rs/tests/integration.rs
+++ rs/tests/integration.rs
@@ -1,17 +1,17 @@
 use bevy::prelude::*;
-use bevy::winit::WinitSettings;
-use bevy_rapier3d::render::RapierDebugRenderPlugin;
 use sond_has::tests::*;
 
 #[bevy_main]
 fn main() {
 	let mut app = app();
+
+	#[cfg(feature = "vis_test")]
 	app.add_system(bevy::window::close_on_esc)
-		.insert_resource(WinitSettings {
+		.insert_resource(bevy::winit::WinitSettings {
 			return_from_run: true,
 			..default()
 		})
-		.add_plugin(RapierDebugRenderPlugin {
+		.add_plugin(bevy_rapier3d::render::RapierDebugRenderPlugin {
 			enabled: true,
 			..default()
 		});
