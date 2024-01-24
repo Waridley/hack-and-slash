a7ecc4455eec49e8e01103e411d357f804d7fc5b
Waridley <Waridley64@gmail.com>
2024-01-23 20:03:20 -0600

    Generate more chunks

--- rs/src/planet/chunks.rs
+++ rs/src/planet/chunks.rs
@@ -1,10 +1,16 @@
 use super::terrain::Ground;
 use crate::planet::PlanetVec2;
 use bevy::{prelude::*, utils::HashMap};
+use bevy_rapier3d::na::Vector3;
 
-pub const CHUNK_SIZE: f32 = 128.0;
-pub const CHUNK_ROWS: usize = CHUNK_SIZE as usize;
-pub const CHUNK_COLS: usize = CHUNK_SIZE as usize;
+pub const CHUNK_ROWS: usize = 128;
+pub const CHUNK_COLS: usize = 128;
+pub const TERRAIN_CELL_SIZE: Vec2 = Vec2::new(16.0, 16.0);
+pub const CHUNK_SCALE: Vector3<f32> = Vector3::new(
+	CHUNK_COLS as f32 * TERRAIN_CELL_SIZE.x,
+	1024.0,
+	CHUNK_ROWS as f32 * TERRAIN_CELL_SIZE.y,
+);
 
 #[derive(Bundle, Debug)]
 pub struct ChunkBundle {
@@ -14,7 +20,7 @@ pub struct ChunkBundle {
 }
 
 #[derive(Component, Debug, Default, Deref, DerefMut)]
-pub struct ChunkCenter(PlanetVec2);
+pub struct ChunkCenter(pub PlanetVec2);
 
 #[derive(Component, Copy, Clone, Default, Debug, Hash, PartialEq, Eq)]
 pub struct ChunkIndex {
@@ -31,8 +37,8 @@ impl ChunkIndex {
 impl From<ChunkIndex> for ChunkCenter {
 	fn from(value: ChunkIndex) -> Self {
 		Self(PlanetVec2::new(
-			value.x as f64 * CHUNK_SIZE as f64,
-			value.y as f64 * CHUNK_SIZE as f64,
+			value.x as f64 * (CHUNK_COLS as f64 * TERRAIN_CELL_SIZE.x as f64),
+			value.y as f64 * (CHUNK_ROWS as f64 * TERRAIN_CELL_SIZE.y as f64),
 		))
 	}
 }
@@ -40,8 +46,8 @@ impl From<ChunkIndex> for ChunkCenter {
 impl From<PlanetVec2> for ChunkIndex {
 	fn from(value: PlanetVec2) -> Self {
 		Self::new(
-			((value.x / CHUNK_SIZE as f64) + 0.5) as i32,
-			((value.y / CHUNK_SIZE as f64) + 0.5) as i32,
+			((value.x / (CHUNK_COLS as f64 * TERRAIN_CELL_SIZE.x as f64)) + 0.5) as i32,
+			((value.y / (CHUNK_ROWS as f64 * TERRAIN_CELL_SIZE.y as f64)) + 0.5) as i32,
 		)
 	}
 }
