0747a2daf6d5e23d58b77cea8b31a2da51dd93a0
Waridley <Waridley64@gmail.com>
2024-02-05 15:30:30 -0600

    Better seed generation

--- rs/src/planet.rs
+++ rs/src/planet.rs
@@ -234,289 +234,140 @@ impl Sub<Vec2> for PlanetVec2 {
 }
 
 pub mod seeds {
-	use crate::planet::terrain;
-	use base64::{engine::general_purpose::URL_SAFE, prelude::*, DecodeError, DecodeSliceError};
+	use base64::{engine::general_purpose::URL_SAFE, prelude::*, DecodeSliceError};
 	use bevy::{
-		log::{error, trace, warn},
 		prelude::Resource,
+		utils::{AHasher, RandomState},
 	};
-	use rand::random;
-	use std::fmt::{Display, Formatter};
+	use rand::{distributions::Standard, prelude::Distribution, Rng};
+	use std::{
+		borrow::Cow,
+		fmt::{Display, Formatter},
+		hash::{BuildHasher, Hash, Hasher},
+	};
 
-	#[derive(Resource, Debug, Copy, Clone, Default, PartialEq, Eq, Hash)]
-	#[repr(C)]
+	#[derive(Resource, Debug, Clone)]
 	pub struct PlanetSeed {
-		pub tera: TerrainSeeds,
+		string: Cow<'static, str>,
+		hash: [u8; 24],
 	}
 
-	impl PlanetSeed {
-		pub const BASE64_CHARS: usize = TerrainSeeds::BASE64_CHARS;
-
-		pub fn base64(self) -> [u8; Self::BASE64_CHARS] {
-			self.tera.base64()
+	impl PartialEq for PlanetSeed {
+		fn eq(&self, other: &Self) -> bool {
+			self.hash == other.hash
 		}
+	}
 
-		pub fn from_base64(bytes: impl AsRef<[u8]>) -> Result<Self, DecodeError> {
-			TerrainSeeds::from_base64(bytes.as_ref()).map(|(tera, rest)| {
-				if rest.len() > 0 {
-					warn!(
-						"Undecoded base64 characters: {}",
-						String::from_utf8_lossy(rest)
-					);
-				}
-				Self { tera }
-			})
+	impl Distribution<PlanetSeed> for Standard {
+		fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> PlanetSeed {
+			let hash = rng.gen::<[u8; 24]>();
+			let string = Cow::from(URL_SAFE.encode(hash));
+
+			PlanetSeed { string, hash }
+		}
+	}
+
+	impl<S: Into<Cow<'static, str>>> From<S> for PlanetSeed {
+		fn from(value: S) -> Self {
+			let string = value.into();
+			let bytes = string.as_bytes();
+			let bytes = if bytes.len() > Self::MAX_INPUT_LEN {
+				bytes.split_at(Self::MAX_INPUT_LEN).0
+			} else {
+				bytes
+			};
+
+			let mut hash = [0u8; 24];
+
+			// Randomly generated during development.
+			// WARNING: Changing these will break compatibility with older seeds.
+			let mut hasher = RandomState::with_seeds(
+				8171301572015878809,
+				11868435174398743204,
+				12156415504192146545,
+				13548443979666687785,
+			)
+			.build_hasher();
+			hasher.write(bytes);
+			hash[0..8].copy_from_slice(&hasher.finish().to_le_bytes());
+
+			hasher.write(bytes);
+			hash[8..16].copy_from_slice(&hasher.finish().to_le_bytes());
+
+			hasher.write(bytes);
+			hash[16..24].copy_from_slice(&hasher.finish().to_le_bytes());
+
+			Self { string, hash }
 		}
 	}
 
 	impl Display for PlanetSeed {
 		fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-			f.write_str(std::str::from_utf8(&self.base64()).unwrap())
+			self.string.fmt(f)
 		}
 	}
 
-	#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-	#[repr(C)]
-	pub struct TerrainSeeds {
-		pub base: u32,
-		perlin: HSSeed,
-		worley: HSSeed,
-		billow: HSSeed,
-		ridged: HSSeed,
-	}
+	impl PlanetSeed {
+		/// Maximum number of characters that will be hashed from the input string.
+		// WARNING: Changing this will break compatibility with some older seeds.
+		pub const MAX_INPUT_LEN: usize = 128;
 
-	const fn base64_chars(bytes: usize) -> usize {
-		bytes.div_ceil(3) * 4
-	}
-	const _ASSERT4: [u8; 8] = [0; base64_chars(4)];
-	const _ASSERT7: [u8; 12] = [0; base64_chars(7)];
-
-	impl TerrainSeeds {
-		/// Number of noise sources.
-		pub const LEN: u8 = 4;
-
-		/// Number of base64 characters to encode [Self::LEN].
-		pub const LEN_CHARS: usize = 4;
-		/// Number of base64 characters to encode [Self::base].
-		pub const BASE_CHARS: usize = base64_chars(4);
-		/// Number of base64 characters to encode all source seeds.
-		pub const SOURCES_CHARS: usize =
-			base64_chars(std::mem::size_of::<HSSeed>() * Self::LEN as usize);
-		/// Total number of base64 characters to encode `Self`.
-		pub const BASE64_CHARS: usize = Self::LEN_CHARS + Self::BASE_CHARS + Self::SOURCES_CHARS;
-
-		pub fn base64(&self) -> [u8; Self::BASE64_CHARS] {
-			let mut ret = [0u8; Self::BASE64_CHARS];
-
-			{
-				let (len, ret) = ret.split_at_mut(Self::LEN_CHARS);
-				URL_SAFE.encode_slice(&[Self::LEN], len).unwrap();
-				let (base, ret) = ret.split_at_mut(Self::BASE_CHARS);
-				URL_SAFE
-					.encode_slice(self.base.to_le_bytes(), base)
-					.unwrap();
-				let sources_bytes = [
-					self.perlin.bytes(),
-					self.worley.bytes(),
-					self.billow.bytes(),
-					self.ridged.bytes(),
-				];
-				let sources_bytes = unsafe {
-					std::mem::transmute::<
-						_,
-						&[u8; Self::LEN as usize * std::mem::size_of::<HSSeed>()],
-					>(&sources_bytes)
-				};
-				let n = URL_SAFE.encode_slice(sources_bytes, ret).unwrap();
-				debug_assert_eq!(n, ret.len());
-			}
-
-			ret
+		pub fn string(&self) -> &Cow<'static, str> {
+			&self.string
 		}
 
-		pub fn from_base64(bytes: &[u8]) -> Result<(Self, &[u8]), DecodeError> {
-			let (len, rest) = bytes.split_at(Self::LEN_CHARS);
-			let mut buf = [u8::MAX; 3];
-			let n = URL_SAFE.decode_slice(len, &mut buf).map_err(|e| match e {
-				DecodeSliceError::DecodeError(e) => e,
-				DecodeSliceError::OutputSliceTooSmall => unreachable!(),
-			})?;
-			debug_assert_eq!(n, 1);
-			let len = buf[0];
-			if len != Self::LEN {
-				warn!("Seed is from a different version. Making a best-effort conversion.");
-			}
+		/// Transforms self into its canonical representation, with the string being the base64
+		/// encoding of its hash.
+		pub fn canonical(mut self) -> Self {
+			self.make_canonical();
+			self
+		}
 
-			let (base, rest) = rest.split_at(Self::BASE_CHARS);
-			let mut buf = [0u8; 4];
-			let n = URL_SAFE.decode_slice_unchecked(base, &mut buf)?;
-			debug_assert_eq!(n, 4);
-			let base = u32::from_le_bytes(buf);
-
-			let sources_chars = usize::min(Self::SOURCES_CHARS, rest.len());
-			let (sources, rest) = rest.split_at(sources_chars);
-			const SOURCES_BYTES: usize = std::mem::size_of::<HSSeed>() * TerrainSeeds::LEN as usize;
-			let mut buf = [u8::MAX; SOURCES_BYTES + 2];
-			match URL_SAFE.decode_slice(sources, &mut buf) {
-				Ok(n) => debug_assert_eq!(n, SOURCES_BYTES),
-				Err(DecodeSliceError::OutputSliceTooSmall) => unreachable!(),
-				Err(DecodeSliceError::DecodeError(e)) => return Err(e),
-			}
-			let buf = unsafe {
-				&*(&buf as *const _
-					as *const [[u8; std::mem::size_of::<HSSeed>()]; Self::LEN as usize])
+		/// Replaces the string representation of self with the canonical base64 encoding of its hash.
+		pub fn make_canonical(&mut self) {
+			let Self { string, hash } = self;
+			let mut canonical = String::with_capacity(32);
+			URL_SAFE.encode_string(hash, &mut canonical);
+			debug_assert_eq!(canonical.len(), 32);
+			if *string != canonical {
+				*string.to_mut() = canonical;
 			};
-
-			Ok((
-				Self {
-					base,
-					perlin: HSSeed::from_bytes(buf[0]),
-					worley: HSSeed::from_bytes(buf[1]),
-					billow: HSSeed::from_bytes(buf[2]),
-					ridged: HSSeed::from_bytes(buf[3]),
-				},
-				rest,
-			))
 		}
 
-		pub fn perlin(&self) -> HSSeed {
-			self.perlin
-		}
-		pub fn worley(&self) -> HSSeed {
-			self.worley
-		}
-		pub fn billow(&self) -> HSSeed {
-			self.billow
-		}
-		pub fn ridged(&self) -> HSSeed {
-			self.ridged
-		}
-	}
-
-	impl Default for TerrainSeeds {
-		fn default() -> Self {
-			let [perlin, worley, billow, ridged] = HSSeed::many_best_effort();
-			Self {
-				base: random(),
-				perlin,
-				worley,
-				billow,
-				ridged,
-			}
-		}
-	}
-
-	/// Combined heights and strength seed for [ChooseAndSmooth](terrain::noise::ChooseAndSmooth)
-	#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-	#[repr(C)]
-	pub struct HSSeed {
-		heights: u32,
-		strength: u32,
-	}
-
-	impl HSSeed {
-		/// Changing the size of `HSSeed` will break compatibility with older seeds.
-		const _ASSERT_SIZE_8: [u8; 8] = [0; std::mem::size_of::<Self>()];
-
-		pub fn try_not_clashing_with(others: &[u32], attempts: usize) -> Option<Self> {
-			let mut s = random();
-			for _ in 0..attempts {
-				if !others.contains(&s) {
-					return Some(Self {
-						heights: random(),
-						strength: s,
-					});
-				} else {
-					trace!("{} clashes with [{}]", URL_SAFE.encode(s.to_le_bytes()), {
-						let mut encoded = String::with_capacity((others.len() * 4).div_ceil(3));
-						for other in others {
-							URL_SAFE.encode_string(other.to_le_bytes(), &mut encoded);
-						}
-						encoded
-					});
-					s = random();
-				}
-			}
-			None
+		pub fn hash(&self) -> &[u8; 24] {
+			&self.hash
 		}
 
-		pub fn best_effort(others: &[u32]) -> Self {
-			const ATTEMPTS: usize = 1024;
-			Self::try_not_clashing_with(others, ATTEMPTS).unwrap_or_else(|| {
-				let s = random();
-				if others.contains(&s) {
-					let mut encoded = String::with_capacity((others.len() * 4).div_ceil(3));
-					for other in others {
-						URL_SAFE.encode_string(other.to_le_bytes(), &mut encoded);
-					}
-					error!(
-						"Couldn't find a non-clashing strength seed in {ATTEMPTS} attempts. \
-					Using {} despite it clashing with [{encoded}].",
-						URL_SAFE.encode(s.to_le_bytes())
-					);
-				} else {
-					trace!(
-						"One last attempt found {}",
-						URL_SAFE.encode(s.to_le_bytes())
-					);
-				}
-				Self {
-					heights: random(),
-					strength: s,
-				}
-			})
-		}
-
-		pub fn many_best_effort<const LEN: usize>() -> [Self; LEN] {
-			let mut ret = [Self {
-				heights: 0,
-				strength: 0,
-			}; LEN];
-			let mut buf = [0; LEN];
-			for i in 0..LEN {
-				let new = Self::best_effort(&buf[0..i]);
-				ret[i] = new;
-				buf[i] = new.strength;
-			}
-			ret
-		}
-
-		pub fn bytes(self) -> [u8; std::mem::size_of::<Self>()] {
-			unsafe {
-				// All std ways of doing this are either unstable or work with slices instead of arrays
-				std::mem::transmute([self.heights.to_le_bytes(), self.strength.to_le_bytes()])
-			}
-		}
-
-		pub fn from_bytes(bytes: [u8; std::mem::size_of::<Self>()]) -> Self {
-			let [heights, strength] = unsafe {
-				// All std ways of doing this are either unstable or work with slices instead of arrays
-				std::mem::transmute(bytes)
-			};
-			Self {
-				heights: u32::from_le_bytes(heights),
-				strength: u32::from_le_bytes(strength),
-			}
-		}
-
-		pub fn heights(&self) -> u32 {
-			self.heights
-		}
-		pub fn strength(&self) -> u32 {
-			self.strength
+		pub fn from_canonical(
+			canonical: impl Into<Cow<'static, str>>,
+		) -> Result<Self, DecodeSliceError> {
+			let string = canonical.into();
+			assert_eq!(string.len(), 32);
+			let mut hash = [0; 24];
+			URL_SAFE.decode_slice(&*string, &mut hash)?;
+			Ok(Self { string, hash })
 		}
 	}
 
 	#[cfg(test)]
 	mod tests {
-		use crate::planet::seeds::PlanetSeed;
+		use super::PlanetSeed;
 
 		#[test]
-		fn seed_base64_round_trip() {
-			let seed = PlanetSeed::default();
-			let encoded = seed.base64();
-			let decoded = PlanetSeed::from_base64(encoded);
-			assert_eq!(Ok(seed), decoded);
+		fn canonical_equivalence() {
+			let seed = PlanetSeed::from("This is a test seed for canonical equivalence.");
+			let (seed, canonical) = (seed.clone(), seed.canonical());
+			assert_ne!(seed.string, canonical.string);
+			assert_eq!(seed, canonical);
+		}
+
+		#[test]
+		fn version_equivalence() {
+			const S: &str = "This is a test seed for seed consistency across game versions";
+			const CANON: &str = "EMVUkEmSHysISOFRcpRyoTWjBDYE3IgV";
+			let seed = PlanetSeed::from(S);
+			let canonical = PlanetSeed::from_canonical(CANON).unwrap();
+			assert_eq!(seed, canonical);
 		}
 	}
 }
